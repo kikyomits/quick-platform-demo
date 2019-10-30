cea1485d5ad9e67e1fea4c61d7955475155ad0b0
mkikyotani <mkikyotani@gmail.com>
2019-10-30 22:59:00 +0900

    add Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://demo-frontend.apps.sandbox.jpn-pe.com"
+    "*"
   );
 
   // Request methods you wish to allow
