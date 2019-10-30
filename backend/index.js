b4b8b3516681037d18d822cafc152a1451f4c181
mkikyotani <mkikyotani@gmail.com>
2019-10-31 01:06:46 +0900

    green

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 });
 
 app.get("/api/v1/color", function(req, res) {
-  res.json("{\"color\": \"red\"}");
+  res.json("{\"color\": \"green\"}");
 });
 
 app.get("/health", function(req, res) {
