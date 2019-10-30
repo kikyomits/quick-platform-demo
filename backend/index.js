457fdb7b7b0441baab62dc5dd36c789b5d31036f
mkikyotani <mkikyotani@gmail.com>
2019-10-30 22:54:38 +0900

    add Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,28 @@ app.get("/health", function(req, res) {
   });
 });
 
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://demo-frontend.apps.sandbox.jpn-pe.com"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //サーバ起動
 app.listen(port);
 console.log("listen on port " + port);
